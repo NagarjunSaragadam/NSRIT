cf04fe554ee52f83f8041ba58a8f32c5e781ccb2
NagarjunSaragadam <thenagarjun558@gmail.com>
2018-02-01 10:23:51 +0530

    Added

--- js/app/app.js
+++ js/app/app.js
@@ -1,113 +1,64 @@
 
   var rendererOptions = {
-  draggable: false
-  };
-  var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-  var directionsService = new google.maps.DirectionsService();
-  var map;
-  var timeout,request;    
-  var heading;
-  var destino = new google.maps.LatLng(18.0008621,-92.9459908);
-  var coords;
-
-  function initialize() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 17.989456, lng: -92.9475061},
-        zoom: 15
-      });
-
-    var infoWindow = new google.maps.InfoWindow({map: map});
-    if (navigator.geolocation) {
-
-      navigator.geolocation.getCurrentPosition(function(position) {
+    draggable: false
+    };
+    var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+    var destino = new google.maps.LatLng(18.0008621,-92.9459908);
+  
+    function initialize() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 17.989456, lng: -92.9475061},
+          zoom: 15
+        });
+  
+      var infoWindow = new google.maps.InfoWindow({map: map});
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        coords = pos.lat + ',' + pos.lng ;			
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(pos.lat, pos.lng),
-          icon: {
-            path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-            scale: 3,            
-            strokeWeight:2,
-            strokeColor:"#B40404"
-         },         
-          map: map
-        });               
-          
-          calcRoute(pos.lat, pos.lng);          
-          //infoWindow.setPosition(pos);
-          //infoWindow.setContent('Location found.');
+  
+          calcRoute(pos.lat, pos.lng);
+  
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
           map.setCenter(pos);
         },
         function() {
           handleLocationError(true, infoWindow, map.getCenter());
         });
-
-
-
-      timeout = setTimeout(function runMe() {
-      navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      heading = google.maps.geometry.spherical.computeHeading
-                     (parseFloat(coords.split(',')[0]), parseFloat(coords.split(',')[1]), new google.maps.LatLng(pos.lat, pos.lng));						
-                     
-        calcRoute(pos.lat, pos.lng);         
-
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(pos.lat, pos.lng),
-          icon: {
-            path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-            scale: 3,
-            rotation: heading,		
-            strokeWeight:2,
-            strokeColor:"#B40404"
-         },         
-          map: map
-        });  
-        coords = pos.lat + ',' + pos.lng ;			
-        //infoWindow.setPosition(pos);
-        //infoWindow.setContent('Location found.');
-        map.setCenter(pos);
-      },
-      function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-      timeout = setTimeout(runMe, 60000);
-      }, 60000);
-    }
-    else {
-    // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-                    'Error: The Geolocation service failed.' :
-                    'Error: Your browser doesn\'t support geolocation.');
-                  }
-    directionsDisplay.setMap(map);
-  }
-
-
-  function calcRoute(lat, lng) {
-
-    var origen = new google.maps.LatLng(lat, lng);
-
-    var request = {
-      origin: origen,
-      destination: destino,
-      travelMode: google.maps.TravelMode.DRIVING
-    };
-    directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
       }
-    });
-  }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
+      else {
+      // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                      'Error: The Geolocation service failed.' :
+                      'Error: Your browser doesn\'t support geolocation.');
+                    }
+      directionsDisplay.setMap(map);
+    }
+  
+  
+    function calcRoute(lat, lng) {
+  
+      var origen = new google.maps.LatLng(lat, lng);
+  
+      var request = {
+        origin: origen,
+        destination: destino,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        }
+      });
+    }
+  
+    google.maps.event.addDomListener(window, 'load', initialize);
