f187da7674a0fa042f077231fe9f62e40e1277a8
SaragadamNagarjun <thenagarjun558@gmail.com>
2018-02-27 22:12:16 +0530

    Edited to root

--- js/app/wright.js
+++ js/app/wright.js
@@ -63,19 +63,27 @@ function Runtimer() {
 function watchPosition() {
    var options = {
       maximumAge: 3600000,
-      timeout: 3000,
+      timeout: 10000,
       enableHighAccuracy: true,
    }
+   var deviceid=device.uuid;
+   var Rootref= firebase.database().ref().child(deviceid);
    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
    function onSuccess(position) {
-      alert('Latitude: '          + position.coords.latitude          + '\n' +
-         'Longitude: '         + position.coords.longitude         + '\n' +
-         'Altitude: '          + position.coords.altitude          + '\n' +
-         'Accuracy: '          + position.coords.accuracy          + '\n' +
-         'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-         'Heading: '           + position.coords.heading           + '\n' +
-         'Speed: '             + position.coords.speed             + '\n' +
-         'Timestamp: '         + position.timestamp                + '\n');
+	   Rootref.set({		   
+		   Cordinates:position.coords.latitude+','+position.coords.longitude
+	   });
+	   var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	   marker.setPosition(latlng);	   
+	   document.getElementById("locationid").innerHTML=position.coords.latitude+','+position.coords.longitude+':'+position.timestamp;
+     //alert('Latitude: '          + position.coords.latitude          + '\n' +
+      //'Longitude: '         + position.coords.longitude         + '\n' +
+      //'Altitude: '          + position.coords.altitude          + '\n' +
+         //'Accuracy: '          + position.coords.accuracy          + '\n' +
+         //'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+         //'Heading: '           + position.coords.heading           + '\n' +
+         //'Speed: '             + position.coords.speed             + '\n' +
+         //'Timestamp: '         + position.timestamp                + '\n');
    };
 
    function onError(error) {
